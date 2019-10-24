c22c299bdabd5d5471ae308f9469c52e592a281d
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-10-24 18:01:22 -0400

    Create beers.py

---
+++ beers.py
@@ -0,0 +1,13 @@
+"""
+Output the lyrics to 99 Bottles of Beer
+"""
+
+import sys
+
+def chorus(counter):
+    print("99 bottles of beer on the wall, 99 bottles of beer."
+    print(Take one down and pass it around, 98 bottles of beer on the wall")
+    return
+
+for i in 99 to 1:
+    chorus(i)
