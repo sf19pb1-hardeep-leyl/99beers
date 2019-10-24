294727251728d5ee79ff626e9386cb2f735935a0
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-10-24 19:59:12 -0400

    Update beers.py

--- beers.py
+++ beers.py
@@ -1,13 +1,19 @@
 """
-Output the lyrics to 99 Bottles of Beer
+Output the lyrics to 99 Bottles of Beer using a function
 """
 
 import sys
 
+line1 = "bottles of beer"
+line2 = "on the wall"
+
 def chorus(counter):
-    print("99 bottles of beer on the wall, 99 bottles of beer."
-    print(Take one down and pass it around, 98 bottles of beer on the wall")
+    print(f"{counter} bottles of beer on the wall, {counter} bottles of beer.")
+    print(f"Take one down and pass it around, {counter-1} bottles of beer on the wall")
     return
 
-for i in 99 to 1:
+for i in range (99,0,-1):
     chorus(i)
+
+print("No more bottles of beer on the wall, no more bottles of beer.")
+print("Go to the store and buy some more, 99 bottles of beer on the wall.")
